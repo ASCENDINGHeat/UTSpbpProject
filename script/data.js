8ffcc35b24adb9be2aa71cee47e362becc138648
ASCENDINGHeat <alfina.jauhar@gmail.com>
2025-10-20 13:41:47 +0700

    Add product data for GT3, LMH, Formula Hybrid, and LMDh packs with details and images

---
+++ script/data.js
@@ -0,0 +1,74 @@
+export const products = {
+    "gt3-pack": {
+        name: "GT3 Pack",
+        price: "$19,99",
+        description: "Experience the thrill of high-performance racing with our GT3 Pack, featuring meticulously crafted cars that deliver unparalleled speed and precision on the track. This pack includes: \n\n- Porsche 911 GT3 R\n- Audi R8 LMS GT3\n- BMW M6 GT3\n- Mercedes-AMG GT3\n- Ferrari 296 GT3\n- Lamborghini Huracán GT3 EVO II\n- And More!",
+        images: [
+            "./assets/img/productImg/GT3pack/packcover.jpg",
+            "./assets/img/productImg/GT3pack/carouselimg (1).jpg",
+            "./assets/img/productImg/GT3pack/carouselimg (2).jpg",
+            "./assets/img/productImg/GT3pack/carouselimg (3).jpg",
+            "./assets/img/productImg/GT3pack/carouselimg (4).jpg",
+            "./assets/img/productImg/GT3pack/carouselimg (5).jpg"
+        ],
+        tags: ["Cars", "GT3", "Racing"],
+        specs: {
+            "Top Speed": "Various",
+            "Engine": "Various",
+            "Power": "500-600 HP"
+        }
+    },
+    
+    "lmh-pack": {
+        name: "LMH Pack",
+        price: "$24,99",
+        description: "Dominate the endurance racing scene with our LMH Pack, featuring cutting-edge Le Mans Hypercars designed for ultimate performance and innovation. This pack includes: \n\n- Toyota GR010 Hybrid\n- Peugeot 9X8\n- Glickenhaus 007 LMH\n- Cadillac V-Series.R\n- And More!",
+        images: [
+            "./assets/img/productImg/LMH/lmh (1).jpg",
+            "./assets/img/productImg/LMH/lmh (2).jpg",
+            "./assets/img/productImg/LMH/lmh (3).jpg"
+        ],
+        tags: ["Cars", "LMH", "Endurance"],
+        specs: {
+            "Top Speed": "330 km/h",
+            "Engine": "Hybrid",
+            "Power": "670 HP"
+        }
+    },
+
+    "formula-hybrid": {
+        name: "Formula Hybrid",
+        price: "$29,99",
+        description: "Experience the pinnacle of open-wheel racing with our Formula Hybrid pack, featuring state-of-the-art hybrid technology for unmatched speed and agility.",
+        images: [
+            "./assets/img/productImg/Formula/img (1).jpg",
+            "./assets/img/productImg/Formula/img (2).jpg",
+            "./assets/img/productImg/Formula/img (3).jpg",
+            "./assets/img/productImg/Formula/img (4).jpg",
+            "./assets/img/productImg/Formula/img (5).jpg"
+        ],
+        tags: ["Cars", "Formula", "Hybrid"],
+        specs: {
+            topSpeed: "350 km/h",
+            engine: "Hybrid V6 Turbo",
+            power: "1000 HP"
+        }
+    },
+
+    "lmdh-pack": {
+        name: "LMDh Pack",
+        price: "$24,99",
+        description: "Conquer the endurance racing world with our LMDh Pack, featuring the latest Le Mans Daytona hybrid prototypes built for speed and efficiency. This pack includes: \n\n- Acura ARX-06\n- BMW M Hybrid V8\n- Cadillac V-LMDh\n- Porsche 963\n- And More!",
+        images: [
+            "./assets/img/productImg/LMDh/lmdh (1).jpg",
+            "./assets/img/productImg/LMDh/lmdh (2).jpg",
+            "./assets/img/productImg/LMDh/lmdh (3).jpg"
+        ],
+        tags: ["Cars", "LMDh", "Endurance"],
+        specs: {
+            "Top Speed": "330 km/h",
+            "Engine": "Hybrid",
+            "Power": "670 HP"
+        }
+    }
+};
